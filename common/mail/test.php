4cfa8606b63eb9b424dbd35873687cca366cf83d
tuhong <466594257@qq.com>
2016-05-12 17:37:59 +0800

    搞出index.php

--- common/mail/test.php
+++ common/mail/test.php
@@ -1,13 +1,19 @@
- <div style="width:600px">
-	<div style="padding:20px 0px 20px 0px;">亲爱的<?php echo $contents['name'];?>：</div>
-	<p style="margin:0px 0px 10px 0px;">请点击下面的链接激活账号完成注册。（48小时内完成）</p>
-	<a href=<?php echo '"'.$contents['link'].'"';?> >
-		<p style="margin:0px 0px 10px 0px;word-wrap: break-word;word-break: break-all;"><?php echo $contents['link'];?></p>
-	</a>
-	<p style="margin:0px 0px 10px 0px;">如果该链接无法点击，请直接复制以上链接到浏览器访问激活。</p>
-	<div style="margin:5px 0px 5px 0px;border-bottom: 1px solid #000000;"></div>
-	<div>
-		<p style="margin:5px 0px 5px 0px;font-size: 12px;">此信息由<a href="http://www.baidu.com">文章网</a>系统发出，请勿直接回复。</p>
-		<p style="margin:5px 0px 5px 0px;font-size: 12px;">如有疑问请<a href="#">联系我们</a>。</p>
-	</div>
+<div style="width:600px">
+    <div style="padding:20px 0px 20px 0px;">
+        <span style="color: red"> 亲爱的</span><?php echo $contents['name']; ?>：
+    </div>
+
+    <p style="margin:0px 0px 10px 0px;">请点击下面的链接激活账号完成注册。（48小时内完成）</p>
+
+    <a href=<?php echo '"' . $contents['link'] . '"'; ?>>
+        <p style="margin:0px 0px 10px 0px;word-wrap: break-word;word-break: break-all;"><?php echo $contents['link']; ?></p>
+    </a>
+    <p style="margin:0px 0px 10px 0px;">如果该链接无法点击，请直接复制以上链接到浏览器访问激活。</p>
+
+    <div style="margin:5px 0px 5px 0px;border-bottom: 1px solid #000000;"></div>
+
+    <div>
+        <p style="margin:5px 0px 5px 0px;font-size: 12px;">此信息由<a href="http://www.baidu.com">文章网</a>系统发出，请勿直接回复。</p>
+        <p style="margin:5px 0px 5px 0px;font-size: 12px;">如有疑问请<a href="#">联系我们</a>。</p>
+    </div>
 </div>
