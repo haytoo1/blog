5cf6351f481576fd1bef2a5da4c2fd97fb5062a5
tuhong <466594257@qq.com>
2016-05-12 17:33:08 +0800

    搞出index.php

--- common/mail/test.php
+++ common/mail/test.php
@@ -1,8 +1,8 @@
  <div style="width:600px">
-	<div style="padding:20px 0px 20px 0px;">亲爱的echo $contents['name'];：</div>
+	<div style="padding:20px 0px 20px 0px;">亲爱的<?php echo $contents['name'];?>：</div>
 	<p style="margin:0px 0px 10px 0px;">请点击下面的链接激活账号完成注册。（48小时内完成）</p>
-	<a href=echo '"'.$contents['link'].'"';>
-		<p style="margin:0px 0px 10px 0px;word-wrap: break-word;word-break: break-all;">echo $contents['link'];</p>
+	<a href=<?php echo '"'.$contents['link'].'"';?> >
+		<p style="margin:0px 0px 10px 0px;word-wrap: break-word;word-break: break-all;"><?php echo $contents['link'];?></p>
 	</a>
 	<p style="margin:0px 0px 10px 0px;">如果该链接无法点击，请直接复制以上链接到浏览器访问激活。</p>
 	<div style="margin:5px 0px 5px 0px;border-bottom: 1px solid #000000;"></div>
