12c03ad9f30e118ae0d5ffa1b3f2561158524402
dj <dj@dengjie-pc>
2016-05-16 16:17:42 +0800

    1

--- frontend/views/index/index.php
+++ frontend/views/index/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="con-text">
 	<a href="article.html" class="article_title">ThinkPHP where方法详解</a>
 	<p>
@@ -45,19 +44,15 @@
 	<a href="#">查看全文</a>
 </div>
 <ul id="jqPaginator"></ul>
-=======
-
-
-					<div class="left-con  col-md-12">
-						<div class="con-text">
-							<a href="<?php echo \yii\helpers\Url::to(['index/article',['id'=>5]]); ?>" class="article_title">ThinkPHP where方法详解</a>
-							<p>
-								<span>分类：<a href="#">PHP</a></span>
-								<span>时间：<time class="entry-date published" datetime="2016-04-22T15:29:27+00:00">2016年4月22日</time></span>
-							</p>
-							<p>在ThinkPHP框架中，where方法的用法是查询语言的精髓，是ThinkPHP连贯操作的重要部分。where方法的参数支持字符串、数组和对象，数组查询是非常强大的，也是官方推荐的。where方法可以完成包括普通查询、表达式查询、快捷查询、区间查询、组合查询在内的查询操作。下面PHP程序员雷雪松详细的讲解下where方法的用法。</p>
-							<a href="<?php echo \yii\helpers\Url::to(['index/article',['id'=>5]]); ?>">查看全文</a>
-						</div>
-						<ul id="jqPaginator"></ul>
-					</div>
->>>>>>> 0241a93ab941b5fc3e8c76f737af2a15d611d482
+	<div class="left-con  col-md-12">
+	<div class="con-text">
+		<a href="<?php echo \yii\helpers\Url::to(['index/article',['id'=>5]]); ?>" class="article_title">ThinkPHP where方法详解</a>
+		<p>
+			<span>分类：<a href="#">PHP</a></span>
+			<span>时间：<time class="entry-date published" datetime="2016-04-22T15:29:27+00:00">2016年4月22日</time></span>
+		</p>
+		<p>在ThinkPHP框架中，where方法的用法是查询语言的精髓，是ThinkPHP连贯操作的重要部分。where方法的参数支持字符串、数组和对象，数组查询是非常强大的，也是官方推荐的。where方法可以完成包括普通查询、表达式查询、快捷查询、区间查询、组合查询在内的查询操作。下面PHP程序员雷雪松详细的讲解下where方法的用法。</p>
+		<a href="<?php echo \yii\helpers\Url::to(['index/article',['id'=>5]]); ?>">查看全文</a>
+	</div>
+	<ul id="jqPaginator"></ul>
+</div>
