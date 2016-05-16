69f36a7e44175460b1202a39da300370f128de8c
tuhong <466594257@qq.com>
2016-05-16 17:09:06 +0800

    登录注册

--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -21,6 +21,10 @@ class IndexController extends yii\web\Controller
         /*if(!yii::$app->getRequest()->getIsAjax()){
             return
         }*/
+        if(yii::$app->getRequest()->getIsAjax()){
+            $userinfo = yii::$app->getSession()->get('username');
+            return ['status'=>1,'msg'=>'ok','userinfo'=>$userinfo,'data'=>[]];
+        }
         return $this->render('index');
 
     }
