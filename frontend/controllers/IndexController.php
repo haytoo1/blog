531b3c857e02bf5ebbe230de5d21c91969c9c49f
tuhong <466594257@qq.com>
2016-05-19 10:54:31 +0800

    登录注册

--- frontend/controllers/IndexController.php
+++ frontend/controllers/IndexController.php
@@ -17,6 +17,35 @@ class IndexController extends yii\web\Controller
     public $keyword = 'PHP入门到精通,MySQL中文文档,nginx,redis,JavaScript,HTML5,iOS,yii2框架,不断学习,超越逗比';
     public function actionIndex()
     {
+
+
+        $ip = $_SERVER['REMOTE_ADDR'];
+
+        if (isset($_SERVER['HTTP_CDN_SRC_IP']) && $_SERVER['HTTP_CDN_SRC_IP']) {
+            $ip = $_SERVER['HTTP_CDN_SRC_IP'];
+        } else {
+            if(!isset($_SERVER['HTTP_X_FORWARDED_FOR'])){
+                return '';
+            }
+            $ip_temp = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            trim($ip_temp);
+            if (strpos($ip_temp, ',')) {
+                $ip_arr_tmp = explode(',', $ip_temp);
+                foreach ($ip_arr_tmp as $_v) {
+//                    if (in_array(trim($_v), $cdnlist) === false) {
+                        $ip .= trim($_v);
+//                    }
+                }
+            } else {
+//                if (in_array($ip_temp, $cdnlist) === false) {
+                    $ip = trim($ip_temp);
+//                }
+            }
+        }
+        file_put_contents('ip.txt',$ip,PHP_EOL);
+//        echo trim($ip);
+        return;
+
         if(yii::$app->getRequest()->getIsAjax()){
             yii::$app->getResponse()->format = 'json';
             return tools::returnDataWithLoginStatus(['status'=>1,'msg'=>'ok','data'=>[]]);
